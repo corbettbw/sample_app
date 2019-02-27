143d5f1570c240109b80b30bb3dc97e3263a80da
Corbett Winningham <corbettbw@gmail.com>
2019-02-27 14:17:11 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # get 'static_pages/...' This does something confusing and messes everything up.
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/contact'
-  get 'static_pages/about'
   root 'static_pages#home'
+  get   '/help',    to: 'static_pages#help'
+  get   '/contact', to: 'static_pages#contact'
+  get   '/about',   to: 'static_pages#about'
+  get   '/signup',  to: 'users#new'
   # root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
