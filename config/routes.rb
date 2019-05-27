dd8db7ff6230d66dc0bdf8c6a92e02f5fc5ecfc0
Corbett Winningham <corbettbw@gmail.com>
2019-05-26 21:52:21 -0700

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
-  # get 'static_pages/...' This does something confusing and messes everything up.
+    # get 'static_pages/...' This does something confusing and messes everything up.
   root 'static_pages#home'
   get   '/help',    to: 'static_pages#help'
   get   '/contact', to: 'static_pages#contact'
   get   '/about',   to: 'static_pages#about'
   get   '/signup',  to: 'users#new'
+  get   '/login',   to: 'sessions#new'
+  post  '/login',   to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
