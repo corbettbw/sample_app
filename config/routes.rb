2ebc1da3bcf27fbedf25daf87f5f192223c29649
Corbett Winningham <corbettbw@gmail.com>
2019-06-12 00:13:44 -0700

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
+#  get 'password_resets/new'
+#  get 'password_resets/edit'
+#  get 'sessions/new'
+#  get 'users/new'
     # get 'static_pages/...' This does something confusing and messes everything up.
   root 'static_pages#home'
   get   '/help',    to: 'static_pages#help'
-  get   '/contact', to: 'static_pages#contact'
   get   '/about',   to: 'static_pages#about'
+  get   '/contact', to: 'static_pages#contact'
   get   '/signup',  to: 'users#new'
   get   '/login',   to: 'sessions#new'
   post  '/login',   to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
